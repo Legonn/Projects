0dee20e8242e463d239bf79c20fa0798639f7be8
Alexandru Botez <88148599+Legonn@users.noreply.github.com>
2022-11-22 13:35:33 +0200

    Final project
    This is my final project for my introduction in It course

---
+++ script.js
@@ -0,0 +1,9 @@
+function afiseazaParagraf1() {
+  document.querySelector(`#Orange`).classList.toggle("element-ascuns");
+}
+function afiseazaParagraf2() {
+  document.querySelector(`#AMS`).classList.toggle("element-ascuns");
+}
+function afiseazaParagraf3() {
+  document.querySelector(`#Yardi`).classList.toggle("element-ascuns");
+}
